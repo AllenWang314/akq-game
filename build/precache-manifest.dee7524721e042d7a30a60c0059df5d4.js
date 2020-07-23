8a5aca0f02e7343e72e33a3efe7fe8c08960a980
Allen Wang <abmathcounts@gmail.com>
2020-07-23 00:39:11 -0400

    pulled things out

---
+++ build/precache-manifest.dee7524721e042d7a30a60c0059df5d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7a49a33af21c599dd3ec19de246888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e396f6380a35780c99c0",
+    "url": "/static/css/main.a15b594c.chunk.css"
+  },
+  {
+    "revision": "65f430b523ffbb65ddd0",
+    "url": "/static/js/2.db637a88.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.db637a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e396f6380a35780c99c0",
+    "url": "/static/js/main.3b78b8f4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
