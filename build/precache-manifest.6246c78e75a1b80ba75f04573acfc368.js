c5f63b37ddb249f316ad11439e1048a2a78fdd3b
Allen Wang <abmathcounts@gmail.com>
2021-07-20 04:23:59 -0700

    ADDING BUILD AHHH

---
+++ build/precache-manifest.6246c78e75a1b80ba75f04573acfc368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1593854c467c37c3ba13ca9563a80820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdc221e0486426a67b1a",
+    "url": "/static/css/main.4fc5ccd4.chunk.css"
+  },
+  {
+    "revision": "45cc2523cb57472f619c",
+    "url": "/static/js/2.b0cfd58e.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.b0cfd58e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdc221e0486426a67b1a",
+    "url": "/static/js/main.5b71f4b1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "df786eabe31e49f28edc093747b48c2a",
+    "url": "/static/media/akq_image.df786eab.png"
+  }
+]);
