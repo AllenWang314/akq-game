51be5049a38df9c19a53066b450733e907273651
Allen Wang <abmathcounts@gmail.com>
2021-07-20 04:25:17 -0700

    breh

--- server/server.go
+++ server/server.go
@@ -4,9 +4,9 @@ import (
 	"log"
 	"net/http"
 	// "strconv"
-	// "strings"
+	"strings"
 	"os"
-	// "path"
+	"path"
 
 	"github.com/AllenWang314/akq-game/socket"
 )
@@ -22,24 +22,41 @@ func Init(port int, isProduction bool) {
 
 	// check if dev or production
 	if isProduction {
+        // frontend build
+		fs := http.FileServer(http.Dir("./build"))
+
 		mux := http.NewServeMux()
-		mux.Handle("/", http.FileServer(http.Dir("build")))
 		mux.Handle("/api/", r)
-	
+
 		// Websocket connection endpoint
 		mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 			socket.ServeWs(hub, w, r)
 		})
-	
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			// If the requested file exists then return if; otherwise return index.html (fileserver default page)
+			log.Println(r.URL.Path)
+			if r.URL.Path != "/" {
+				fullPath := "./build/" + strings.TrimPrefix(path.Clean(r.URL.Path), "/")
+				_, err := os.Stat(fullPath)
+				if err != nil {
+					if !os.IsNotExist(err) {
+						panic(err)
+					}
+					// Requested file does not exist so we return the default (resolves to index.html)
+					r.URL.Path = "/"
+				}
+			}
+			log.Println(r.URL.Path)
+			fs.ServeHTTP(w, r)
+		})
+
+		// start server
 		addr := ":" + os.Getenv("PORT")
-	
-	
-		// Start the server
-		// fmt.Println("Serving at", addr)
+		log.Println("Serving at", addr)
 		err := http.ListenAndServe(addr, mux)
-	
+
 		if err != nil {
-			// fmt.Println("yeet")
+			log.Println("error in http listen and serve")
 			panic(err)
 		}
     } else {
