5e5a817b7d7cbf7bfb04233e2eee2f7670f506bc
Allen Wang <abmathcounts@gmail.com>
2021-07-20 04:14:47 -0700

    trying to use last year's code

--- server/server.go
+++ server/server.go
@@ -22,42 +22,24 @@ func Init(port int, isProduction bool) {
 
 	// check if dev or production
 	if isProduction {
-        // frontend build
-		fs := http.FileServer(http.Dir("./build"))
-
 		mux := http.NewServeMux()
-		mux.Handle("/", fs)
+		mux.Handle("/", http.FileServer(http.Dir("build")))
 		mux.Handle("/api/", r)
-
+	
 		// Websocket connection endpoint
 		mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 			socket.ServeWs(hub, w, r)
 		})
-		// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// 	// If the requested file exists then return if; otherwise return index.html (fileserver default page)
-		// 	log.Println(r.URL.Path)
-		// 	if r.URL.Path != "/" {
-		// 		fullPath := "./build/" + strings.TrimPrefix(path.Clean(r.URL.Path), "/")
-		// 		_, err := os.Stat(fullPath)
-		// 		if err != nil {
-		// 			if !os.IsNotExist(err) {
-		// 				panic(err)
-		// 			}
-		// 			// Requested file does not exist so we return the default (resolves to index.html)
-		// 			r.URL.Path = "/"
-		// 		}
-		// 	}
-		// 	log.Println(r.URL.Path)
-		// 	fs.ServeHTTP(w, r)
-		// })
-
-		// start server
+	
 		addr := ":" + os.Getenv("PORT")
-		log.Println("Serving at", addr)
+	
+	
+		// Start the server
+		// fmt.Println("Serving at", addr)
 		err := http.ListenAndServe(addr, mux)
-
+	
 		if err != nil {
-			log.Println("error in http listen and serve")
+			// fmt.Println("yeet")
 			panic(err)
 		}
     } else {
