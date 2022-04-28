c5d7e512b8d6543af1b22a0a5a1d95cf80d19246
Allen Wang <abmathcounts@gmail.com>
2022-04-28 02:32:23 -0400

    added default port

--- server/main.go
+++ server/main.go
@@ -1,30 +1,31 @@
 package main
 
-import( 
-    "fmt"
-    "flag"
-    "os"
-    "strings"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
 
-    "github.com/AllenWang314/akq-game/config"
-    "github.com/AllenWang314/akq-game/server"
-    "github.com/AllenWang314/akq-game/db"
+	"github.com/AllenWang314/akq-game/config"
+	"github.com/AllenWang314/akq-game/db"
+	"github.com/AllenWang314/akq-game/server"
 )
 
 func main() {
-    environment := flag.String("e", "dev", "")
-    port :=  flag.Int("p", 8080, "")
-    reset := flag.Bool("reset", false, "Resets the database")
+	environment := flag.String("e", "dev", "")
+	config.Init(*environment)
 
-    flag.Usage = func() {
+	port := flag.Int("p", config.GetConfig().GetInt("server.addr"), "")
+	reset := flag.Bool("reset", false, "Resets the database")
+
+	flag.Usage = func() {
 		fmt.Println("Usage: server -e {mode} -p {port}")
 		os.Exit(1)
 	}
 
-    flag.Parse()
-  
-    isProduction := strings.HasPrefix(*environment, "prod")
-    config.Init(*environment)
-    db.Init(*reset, isProduction)
-    server.Init(*port, isProduction)
+	flag.Parse()
+
+	isProduction := strings.HasPrefix(*environment, "prod")
+	db.Init(*reset, isProduction)
+	server.Init(*port, isProduction)
 }
